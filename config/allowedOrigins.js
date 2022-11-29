c96fafb308ac564558a89df5b18f8d07c1b6a931
Abhishek-baj1289 <114385523+Abhishek-baj1289@users.noreply.github.com>
2022-11-29 17:37:49 +0530

    updated allowed origins

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,7 +1,6 @@
 const allowedOrigins = [
-    'https://www.abhishekbajpai.com',
-    'http://abhishekbajpai.com',
     'https://technotes-xrw3.onrender.com',
-    'http://technotes-xrw3.onrender.com'
+    'http://technotes-xrw3.onrender.com',
+    'https://technotes-xrw3.onrender.com/'
 ]
 module.exports = allowedOrigins; 
